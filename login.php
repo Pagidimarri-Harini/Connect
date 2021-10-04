d5ddea33433243775ed8693da671f3695925d8a5
pagidimarri harini <pagidimarriharini66@gmail.com>
2021-10-04 12:01:01 +0000

    Upload New File

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include "db_conn.php";
+if(isset($_POST['username']) && isset($_POST['password'])){
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    $username = validate($_POST['username']);
+    $password = validate($_POST['password']);
+    if(empty($username)){
+        header("Location:index.php?error = username is required");
+        exit();
+        
+
+    }else if(empty($password)){
+        header("Location:index.php?error = password is required");
+        exit();
+    }else{
+        $sql = "SELECT * FROM users WHERE username='$username' AND password ='$password'";
+        $result = mysqli_query($conn,$sql);
+        if (mysqli_num_rows($result)===1) {
+            $row = mysqli_fetch_assoc($result);
+            if($row['username'] === $username && $row['password']=== $password){
+                $SESSION['username']=$row['username'];
+                $SESSION['name']=$row['name'];
+                $SESSION['id']=$row['id'];
+                header("Location:home.html");
+                exit();
+            }else{
+                header("Location:index.php?error = Incorrect Username or password");
+                exit();
+    
+            }
+        }else{
+            header("Location:index.php?error = Incorrect Username or password");
+            exit();
+        }
+
+    }
+    
+    
+
+}else{
+    header("Location: index.php");
+    exit();
+}
+
+?>
