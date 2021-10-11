f1b1e7e8c1b892c97414df80d0049099237a94c4
ksk-dgkr <kaushikadegekar@gmail.com>
2021-10-11 14:54:44 +0000

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -5,7 +5,7 @@ $uname = "root";
 $password = "";
 $db_name = "test_db";
 
-$conn = mysql_connect($sname, $uname, $password, $db_name);
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
 	echo "Connection failed!";
